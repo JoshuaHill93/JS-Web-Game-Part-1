d80fbd3ea92395035eff0c1348642c8f9b6c958a
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-09-19 15:36:55 -0700

    added some images

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage (url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url 
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+}
+
+newImage('assets/green-character.gif',100,100)
+newImage('assets/pine-tree.png', 450,200)
+newImage('assets/tree.png',200,300)
+newImage('assets/pillar.png',350,100)
+newImage('assets/crate.png',150,200)
+newImage('assets/well.png',500,425)
+
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
+
+
+
