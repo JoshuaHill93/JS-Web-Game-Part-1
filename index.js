0c4c10d173ff556f84a09a2d748a8bd96028ef34
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-09-20 15:26:11 -0700

    nearly done cant click image

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ function newImage (url, left, bottom){
     object.style.position = 'fixed'
     object.style.left = left + 'px'
     object.style.bottom = bottom + 'px'
-    document.body.append(object)
+    document.body.append(object,)
     return object
 }
 function newItem(url, left, bottom){
     let object = newImage(url, left, bottom)
 
+    object.addEventListener('click',function(){
+        object.remove()
+    })
 }
+
+
+
+
 
 newImage('assets/green-character.gif',100,100)
 newImage('assets/pine-tree.png', 450,200)
@@ -19,18 +26,12 @@ newImage('assets/tree.png',200,300)
 newImage('assets/pillar.png',350,100)
 newImage('assets/crate.png',150,200)
 newImage('assets/well.png',500,425)
+newImage('assets/sword.png',500,405)
+newImage('assets/shield.png',165,185)
+newImage('assets/staff.png',600,100,)
 
 
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
 
-sword.addEventListener('click', function(){
-    sword.remove()
-})
 
 
 
